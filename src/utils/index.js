f689352e42f654ad8608a37e54b647df25e101d0
Coder-King3 <2196592083@qq.com>
2023-10-23 01:49:00 +0800

    day and night themes are automatically set according to time

--- src/utils/index.js
+++ src/utils/index.js
@@ -43,3 +43,56 @@ export function debounce(callback, delay = 0, immediate = false) {
 export function getAssetsUrl(url, suffix = 'png', folder = 'icons') {
   return new URL(`../assets/${folder}/${url}.${suffix}`, import.meta.url).href
 }
+
+export function formatTimer(cellValue, foramtType = 'yyyy-MM-dd HH:mm:ss') {
+  if (!cellValue) return new Date()
+
+  const date = new Date(cellValue)
+  function procesWeek(weekNum) {
+    weekNum = weekNum === 0 ? 7 : weekNum
+    const WeekList = [
+      { weekNum: 1, weekStr: '一' },
+      { weekNum: 2, weekStr: '二' },
+      { weekNum: 3, weekStr: '三' },
+      { weekNum: 4, weekStr: '四' },
+      { weekNum: 5, weekStr: '五' },
+      { weekNum: 6, weekStr: '六' },
+      { weekNum: 7, weekStr: '日' }
+    ]
+    const { weekStr } = WeekList.find((item) => item.weekNum === weekNum)
+    return {
+      weekNum,
+      weekStr
+    }
+  }
+
+  let dateTimer
+
+  if (foramtType && foramtType.trim() != '' && (dateTimer = foramtType)) {
+    const formatArray = [
+      { rule: 'yyyy', value: timer['year'] },
+      { rule: 'MM', value: timer['month'] },
+      { rule: 'dd', value: timer['day'] },
+      { rule: 'HH', value: timer['hours'] },
+      { rule: 'mm', value: timer['minutes'] },
+      { rule: 'ss', value: timer['seconds'] },
+      { rule: 'W', value: timer['week'] }
+    ]
+    formatArray.forEach(({ rule, value }) => {
+      dateTimer = dateTimer.replaceAll(rule, value)
+    })
+  } else {
+    dateTimer = {
+      year: `${date.getFullYear()}`,
+      month: `${date.getMonth() + 1}`.padStart(2, '0'),
+      day: `${date.getDate()}`.padStart(2, '0'),
+      hours: `${date.getHours()}`.padStart(2, '0'),
+      minutes: `${date.getMinutes()}`.padStart(2, '0'),
+      seconds: `${date.getSeconds()}`.padStart(2, '0'),
+      week: procesWeek(date.getDay()).weekStr,
+      weekNum: procesWeek(date.getDay()).weekNum
+    }
+  }
+
+  return dateTimer
+}
