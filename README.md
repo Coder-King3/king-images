ae0f01b86c565b0fd9b20ad06ccd236636a7b994
Coder-King3 <2196592083@qq.com>
2023-11-17 09:25:37 +0800

    deleted all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# king-images
-一个使用mini-vue构建的web图床应用程序
--- main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'king-minivue'
-
-import './src/assets/styles/index.less'
-import App from './src/app.js'
-
-const app = createApp(App)
-app.mount('#root')
