90019995284f8a20713f3456e47a8f449b5e9ece
Coder-King3 <2196592083@qq.com>
2023-11-28 10:56:45 +0800

    📦️build: 更换图片预览和消息提示库

--- src/components/content/cpns/image-list/index.jsx
+++ src/components/content/cpns/image-list/index.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import './style.less'
-import { msgSuccess, msgError } from '~/package_modules/message'
-import preview from '~/package_modules/preview'
+import React, { useContext } from 'react'
+import Zmage from 'react-zmage'
+import 'react-zmage/lib/zmage.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { getAssetsUrl } from '@/utils'
+import { themeContext } from '@/context'
+import './style.less'
 
 const ImageList = () => {
   const images = window.baseConfig.images
+  const [mode] = useContext(themeContext)
 
   const copyImageUrl = (url) => {
     navigator.clipboard
       .writeText(url)
-      .then(() => msgSuccess('复制成功！'))
-      .catch(() => msgError('复制失败！'))
+      .then(() => toast.success('复制成功！', { theme: mode }))
+      .catch(() => toast.error('复制失败！', { theme: mode }))
   }
-  const previewImage = (imageSelect) => {
-    const imageEvent = document.querySelector(imageSelect)
-    preview(imageEvent)
+  const previewImage = (url) => {
+    Zmage.browsing({
+      src: url,
+      backdrop: mode == 'light' ? '#f8f9fa' : '#2f2f2f'
+    })
   }
 
   return (
     <ul className="image-list">
       {images.map((url, index) => (
         <li className="item" key={`item_${index}`}>
-          <img src={url} className={`image preview-item_${index}`} />
+          <img src={url} className="image" />
           <div className="tools">
             <img
               src={getAssetsUrl('zoom-in', 'svg', 'icons')}
               className="svg-icon"
-              onClick={() => previewImage(`.preview-item_${index}`)}
+              onClick={() => previewImage(url)}
             />
             <img
               src={getAssetsUrl('copy', 'svg', 'icons')}
@@ -38,8 +44,19 @@ const ImageList = () => {
         </li>
       ))}
       {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
-        <i className={`flex-i-box i-box_${index}`} key={`fiex_i_${index}`}></i>
+        <i className={`flex-i-box`} key={`fiex_i_${index}`}></i>
       ))}
+      <ToastContainer
+        position="top-center"
+        autoClose={500}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+      />
     </ul>
   )
 }
